da5e7d3770d3a7760fd770a648c91b24a4010d20
Pedro Velasco <pedro@pedro.com>
2015-10-19 19:14:31 +0200

    Version 3 -Ultimo numero -Checkbox para desactivarlo -Cambio grafico

--- app/src/main/java/com/pedrovelasco/dados_hechoscuriosos3/MainActivity.java
+++ app/src/main/java/com/pedrovelasco/dados_hechoscuriosos3/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -16,26 +17,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final TextView factnum = (TextView) findViewById(R.id.textView);
+        final TextView lastnum = (TextView) findViewById(R.id.ultimo_numero);
+        final TextView lbl_ul = (TextView) findViewById(R.id.lbl_ul);
+
         Button b4 = (Button) findViewById(R.id.btn_4);
         Button b6 = (Button) findViewById(R.id.btn_6);
         Button b8 = (Button) findViewById(R.id.btn_8);
         Button b10 = (Button) findViewById(R.id.btn_10);
         Button b20 = (Button) findViewById(R.id.btn_20);
         Button b12 = (Button) findViewById(R.id.btn_12);
+        final CheckBox cb_mostrar = (CheckBox) findViewById(R.id.cb_mostrar);
 
         b4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                lastnum.setText(factnum.getText());
                 String num = "";
                 Random randomGenenerator = new Random();
                 int randomNumber = randomGenenerator.nextInt(4);
                 num = Integer.toString(randomNumber+ 1);
                 factnum.setText(num);
+
+
+
             }
         });
         b6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                lastnum.setText(factnum.getText());
                 String num = "";
                 Random randomGenenerator = new Random();
                 int randomNumber = randomGenenerator.nextInt(6);
@@ -47,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         b8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                lastnum.setText(factnum.getText());
                 String num = "";
                 Random randomGenenerator = new Random();
                 int randomNumber = randomGenenerator.nextInt(8);
@@ -58,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
         b10.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                lastnum.setText(factnum.getText());
                 String num = "";
                 Random randomGenenerator = new Random();
                 int randomNumber = randomGenenerator.nextInt(10);
@@ -69,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
         b12.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                lastnum.setText(factnum.getText());
                 String num = "";
                 Random randomGenenerator = new Random();
                 int randomNumber = randomGenenerator.nextInt(12);
@@ -80,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
         b20.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                lastnum.setText(factnum.getText());
                 String num = "";
                 Random randomGenenerator = new Random();
                 int randomNumber = randomGenenerator.nextInt(20);
@@ -87,6 +101,26 @@ public class MainActivity extends AppCompatActivity {
                 factnum.setText(num);
             }
         });
+
+        cb_mostrar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if(!cb_mostrar.isChecked()){
+                    lbl_ul.setVisibility(v.GONE);
+                    lastnum.setVisibility(v.GONE);
+                }else{
+                    lastnum.setVisibility(v.VISIBLE);
+                    lbl_ul.setVisibility(v.VISIBLE);
+                }
+
+
+            }
+
+
+
+        });
+
+
+
 
 
 
